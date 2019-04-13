18ccba1bf9d56a0327c7bb43201d849bdd56b607
Andrew Tiller <tiller.andrew@gmail.com>
2019-04-13 13:27:30 -0500

    removed console logs from orderBuilder and moved variables into function so order was reset on each button-push. added util.js with printToDom function

--- javascripts/components/orderBuilder.js
+++ javascripts/components/orderBuilder.js
@@ -1,24 +1,23 @@
 // Helpers
 import breadInfo from '../helpers/bread.js';
 import orderString from '../helpers/orderString.js';
-
-// Variables
-const breads = breadInfo.getBreadInfo();
-const order = [];
+import util from '../helpers/util.js';
 
 const buildOrder = (e) => {
     e.preventDefault();
+    const breads = breadInfo.getBreadInfo();
+    let order = [];
+
     let checkBoxes = Array.from(document.getElementsByClassName('form-check-input'));
     checkBoxes.forEach((box) => {
         if (box.checked) {
-            console.log(`${box.id} is checked`);
-            console.log(`bread.${box.id} returns ${breads[box.id]}`);
+            // console.log(`${box.id} is checked`);
+            // console.log(`bread.${box.id} returns ${breads[box.id]}`);
             order.push([box.id, breads[box.id]]);
         }
     });
     console.log(order);
-    const selectedDiv = document.getElementById('orderDiv');
-    selectedDiv.innerHTML = orderString.orderStringBuilder(order);
+    util.printToDom('orderDiv', orderString.orderStringBuilder(order));
 };
 
 export default { buildOrder };
